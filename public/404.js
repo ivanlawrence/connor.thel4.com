ec2e6d79de6713c999c1f1314138b9249597b8e9
Connor <connor@theL4.com>
2021-06-08 19:14:49 +0000

    makes 404 able to change urls

---
+++ public/404.js
@@ -0,0 +1,8 @@
+const urlRequest = document.getElementById("input");
+function goToURL() {
+    window.location.href = urlRequest.value;
+}
+
+function homePage() {
+    window.location.href = "http://connor.thel4.com";
+}
