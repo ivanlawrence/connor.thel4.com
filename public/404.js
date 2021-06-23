33021c7b0e4fbdccc55ad81db6b281f4b6101aa5
Connor <connor@theL4.com>
2021-06-23 20:58:09 +0000

    finishes 404

--- public/404.js
+++ public/404.js
@@ -1,7 +1,10 @@
 const urlRequest = document.getElementById("input");
-function goToURL() {
-    window.location.href = urlRequest.value;
-}
+const link = document.getElementById("link");
+
+urlRequest.addEventListener('input', function(){
+    link.href = urlRequest.value;
+    link.innerText = urlRequest.value;
+});
 
 function homePage() {
     window.location.href = "http://connor.thel4.com";
