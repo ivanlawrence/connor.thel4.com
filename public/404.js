a98151ef0efc6e973288089061aae434ca806ab1
Connor <connor@theL4.com>
2021-07-29 22:40:05 +0000

    updates buttons

--- public/404.js
+++ public/404.js
@@ -1,11 +1,13 @@
 const urlRequest = document.getElementById("input");
 const link = document.getElementById("link");
+document.getElementById("goHome").addEventListener("click", homePage );
 
-urlRequest.addEventListener('input', function(){
+urlRequest.addEventListener('input', function() {
     link.href = urlRequest.value;
-    link.innerText = urlRequest.value + "*";
+    link.innerHTML = urlRequest.value + "*";
 });
 
+document.getElementById("goHome").addEventListener("click", homePage );
 function homePage() {
     window.location.href = "http://connor.thel4.com";
 }
