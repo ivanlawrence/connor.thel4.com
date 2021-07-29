f45b21446caf6cae5a59aa8c8d602582a57e478a
Connor <connor@theL4.com>
2021-07-29 22:41:29 +0000

    makes functions work in poker and adds home button

--- public/poker/poker.js
+++ public/poker/poker.js
@@ -110,3 +110,9 @@ const functions = {
     }
 }
 functions.setup()
+
+document.getElementById("goHome").addEventListener("click", homePage );
+
+function homePage() {
+    window.location.href = "http://connor.thel4.com";
+}
