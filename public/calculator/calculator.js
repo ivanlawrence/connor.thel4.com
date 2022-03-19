966fb6593e4f9b2c1c6934273efbf5375da2dc1c
Connor Lawrence <cfindalylawrence@gmail.com>
2022-03-19 00:31:39 +0000

    adds and uses Vue to assist code (keeping the original js)

--- public/calculator/calculator.js
+++ public/calculator/calculator.js
@@ -1,46 +1,31 @@
 document.getElementById("go").addEventListener("click", doMath );
+const opvar = document.getElementById("oporator");
+opvar.addEventListener("onchange", getOporatorName );
 
 function doMath() {
-  const int1st = document.getElementById('int1st').value;
-  const oporator = document.getElementById('oporators').value;
-  const int2nd = document.getElementById('int2nd').value;
+  const int1st = document.getElementById("int1st").value;
+  const oporator = document.getElementById("oporators").value;
+  const int2nd = document.getElementById("int2nd").value;
 
-  let answer = '';
-  if (oporator === "+") answer = getSum(int1st, int2nd);
-  if (oporator === "-") answer = getDiffrence(int1st, int2nd);
-  if (oporator === "*") answer = getProduct(int1st, int2nd);
-  if (oporator === "/") answer = getQuotient(int1st, int2nd);
+  let answer = "";
+  
+  if (oporator === "+") answer = int1st + int2nd;
+  else if (oporator === "-") answer = int1st - int2nd;
+  else if (oporator === "*") answer = int1st * int2nd;
+  else if (oporator === "/") answer = int1st / int2nd;
+  else "poop";
 
-  document.getElementById('answer').value = answer;
+  document.getElementById("answer").value = answer;
 }
 
-function getSum(a, b) {
-  const intA = Number.parseInt(a);
-  const intB = Number.parseInt(b);
-  return intA + intB;
-}
-
-function getDiffrence(a, b) {
-  return a - b;
-}
-
-function getProduct(a, b) {
-  return a * b;
-}
-
-function getQuotient(a, b) {
-  return a / b;
-
-}
-
-function getOporatorName(el) {
-  const oporator = el.value
-  let opName = '';
-  if (oporator === "+") opName = 'Sum';
-  if (oporator === "-") opName = 'Diffrence';
-  if (oporator === "*") opName = 'Prouduct';
-  if (oporator === "/") opName = 'Quotient';
-  document.getElementById('answer').value = opName;
+function getOporatorName() {
+  const oporator = opvar.value
+  let opName = "";
+  if (oporator === "+") opName = "Sum";
+  if (oporator === "-") opName = "Diffrence";
+  if (oporator === "*") opName = "Prouduct";
+  if (oporator === "/") opName = "Quotient";
+  document.getElementById("answer").value = opName;
 }
 
 document.getElementById("goHome").addEventListener("click", homePage );
---
+++ public/calculator/vue-cal.js
@@ -0,0 +1,40 @@
+const vueApp = new Vue({
+
+    el:"#vue-app",
+
+    data:{
+        int1:"",
+        int2:"",
+        ans:"",
+        oporator:"",
+        oporators:[
+            {
+                displayName:"Addition",
+                value:"+"
+            },
+            {
+                displayName:"Subtraction",
+                value:"-"
+            },
+            {
+                displayName:"Multiplication",
+                value:"*"
+            },
+            {
+                displayName:"Division",
+                value:"/"
+            },
+        ],
+    },
+
+    methods:{
+
+    },
+
+    computed:{
+        addition() { return  parseInt(this.int1) + parseInt(this.int2); },
+        subtraction() { return this.int1 - this.int2; },
+        multiplication() { return this.int1 * this.int2; },
+        division() { return this.int1 / this.int2; },
+    },
+});
