59fe9e64aaa1fa64a32e844afd72838866237046
Connor Lawrence <connor@thel4.com>
2023-02-11 18:06:42 +0000

    Touched up on calculator, deleted tic-tac-toe and fixed 404

--- public/404.js
+++ public/404.js
@@ -1,12 +1,5 @@
-const urlRequest = document.getElementById("input");
-const goBtn = document.getElementById("go");
 document.getElementById("goHome").addEventListener("click", homePage );
 
-go.addEventListener('click', function() {
-    window.location.href = urlRequest.value;
-});
-
-document.getElementById("goHome").addEventListener("click", homePage );
 function homePage() {
     window.location.href = "http://connor.thel4.com";
 }
