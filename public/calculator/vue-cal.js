4470d5b577f6b3ad7b6e33a6a6f44237224db839
Connor Lawrence <connor@thel4.com>
2023-02-11 19:12:22 +0000

    Fixes go home

--- public/calculator/vue-cal.js
+++ public/calculator/vue-cal.js
@@ -44,3 +44,9 @@ document.getElementById("goHome").addEventListener("click", homePage );
 function homePage() {
     window.location.href = "http://connor.thel4.com";
 }
+
+document.getElementById("goHome").addEventListener("click", homePage );
+
+function homePage() {
+    window.location.href = "http://connor.thel4.com";
+}
